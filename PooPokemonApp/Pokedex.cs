781c8fd306c17364c4a3fe9e3685a88d3f191635
Joao Vitor Alves Lima <joao-mex@hotmail.com>
2022-03-31 17:57:47 -0300

    Battle mode inserted

--- PooPokemonApp/Pokedex.cs
+++ PooPokemonApp/Pokedex.cs
@@ -9,27 +9,29 @@ namespace PooPokemonApp
     public class Pokedex
     {
 
-        private List<Pokemon> pokemons;
+        private List<PokemonPlus> pokemons;
 
         public Pokedex()
         {
             this.InitializeList();
         }
 
-        public List<Pokemon> Pokemons
+        public List<PokemonPlus> Pokemons
         {
             get { return pokemons; }
         }
 
-        public void Show()
+        private void Show()
         {
             Menu();
         }
         public void Menu()
         {
-            Console.WriteLine("\nPokedex----Pokemon schedule");
-            Console.WriteLine("0 - Exit pokedex");
-            Console.WriteLine("1 - List all pokemons");
+            Console.WriteLine("\nPokemon -- Battle game");
+            Console.WriteLine("0 - Exit game");
+            Console.WriteLine("1 - List all pokemons in pokedex");
+            Console.WriteLine("2 - Battle");
+
             Console.Write("What do yout want to do? ");
             var option = int.Parse(Console.ReadLine());
                 switch (option)
@@ -40,7 +42,23 @@ namespace PooPokemonApp
                             Environment.Exit(0);
                             break;
                         }
-                    case 1: ListPokemons(); break;
+                    case 1:
+                    {
+                        ListPokemons();
+                        Console.ReadKey();
+                        Console.Clear();
+                        Show();
+                        break;
+                    }
+                    case 2:
+                    {
+                        Battle();
+                        Console.ReadKey();
+                        Console.Clear();
+                        Show();
+                        break;
+                    }
+
                     default:
                     {
                         Console.Clear();
@@ -53,48 +71,81 @@ namespace PooPokemonApp
         private void InitializeList()
         {
             // Instanciar a lista
-            this.pokemons = new List<Pokemon>();
-            Pokemon p = new Pokemon("Bulbasaur", "Grass type pokemon");
+            this.pokemons = new List<PokemonPlus>();
+            PokemonPlus p = new PokemonPlus("Bulbasaur", "Grass type pokemon", 60);
             this.pokemons.Add(p);
-            p = new Pokemon("Mew", "Psychic legendary pokemon");
+            p = new PokemonPlus("Mew", "Psychic legendary pokemon", 90);
             this.pokemons.Add(p);
-            p = new Pokemon("Mewtwo", "Psychic legendary pokemon");
+            p = new PokemonPlus("Mewtwo", "Psychic legendary pokemon", 70);
             this.pokemons.Add(p);
-            p = new Pokemon("Giratina", "Ghost legendary pokemon");
+            p = new PokemonPlus("Giratina", "Ghost legendary pokemon", 95);
             this.pokemons.Add(p);
-            p = new Pokemon("Flyhon", "Your description");
+            p = new PokemonPlus("Flygon", "Dragon type pokemon", 55);
             this.pokemons.Add(p);
-            p = new Pokemon("Metagross", "Your description");
+            p = new PokemonPlus("Metagross", "Toxic type pokemon", 11);
             this.pokemons.Add(p);
-            p = new Pokemon("Muk", "Your description");
+            p = new PokemonPlus("Muk", "Steel type pokemon", 50);
             this.pokemons.Add(p);
-            p = new Pokemon("Pikachu", "Eletric type pokemon");
+            p = new PokemonPlus("Pikachu", "Eletric type pokemon", 60);
             this.pokemons.Add(p);
-            p = new Pokemon("Entei", "Fire legendary pokemon");
+            p = new PokemonPlus("Entei", "Fire legendary pokemon", 165);
             this.pokemons.Add(p);
-            p = new Pokemon("Lucario", "Fighter and steel type pokemon");
+            p = new PokemonPlus("Lucario", "Fighter and steel type pokemon", 65);
             this.pokemons.Add(p);
-            p = new Pokemon("Mudkip", "Water type pokemon");
+            p = new PokemonPlus("Mudkip", "Water type pokemon", 35);
             this.pokemons.Add(p);
-            p = new Pokemon("Paras", "Insect type pokemon");
+            p = new PokemonPlus("Paras", "Insect type pokemon", 33);
             this.pokemons.Add(p);
-            p = new Pokemon("Kyogre", "Water legendary pokemon");
+            p = new PokemonPlus("Kyogre", "Water legendary pokemon", 89);
             this.pokemons.Add(p);
         }
 
-        public void ListPokemons()
+        private void ListPokemons()
         {
+            Console.Clear();
             Console.WriteLine("Pokemons list:\n");
             for(int i = 0; i < this.pokemons.Count; i++)
             {
+                Console.WriteLine("Pokemon code: " + i);
                 this.Pokemons[i].ShowPokemonData();
                 //Pokemon p = this.pokemons[i];
                 //p.ShowPokemonData();
                 Console.WriteLine("-----------------------------------");
             }
-            Console.ReadKey();
-            Console.Clear();
-            Show();
+        }
+
+        private void Battle()
+        {
+            ListPokemons();
+            Console.WriteLine("Enter pokemon code: ");
+            var code = Convert.ToInt32(Console.ReadLine());
+            //pegar o pokemon do player
+            var pPlayer = this.Pokemons[code];
+            Console.WriteLine("You chose: " + pPlayer.Name);
+            //definir o pokemon do pc
+            Random r = new Random();
+            code = r.Next(0, Pokemons.Count);
+            var pPC = this.Pokemons[code];
+            Console.WriteLine("\nPC chose: " + pPC.Name);
+            //batalhar
+            if (pPlayer.Power >= pPC.Power)
+            {
+                Console.Clear();
+                Console.WriteLine("\nCongratulations!!! You win");
+                Console.WriteLine("Your pokemon is stronger than your opponent's pokemon. " + "\nYour choice:");
+                pPlayer.ShowPokemonDataPlus();
+                Console.WriteLine("\nThe opponent choose: ");
+                pPC.ShowPokemonDataPlus();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("What a sadness!!! You lose");
+                Console.WriteLine("Your pokemon is weaker than your opponent's pokemon. " + "\nYour choice:");
+                pPlayer.ShowPokemonDataPlus();
+                Console.WriteLine("\nThe opponent choose: ");
+                pPC.ShowPokemonDataPlus();
+            }
         }
 
 
---
+++ PooPokemonApp/PokemonPlus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooPokemonApp
+{
+    public class PokemonPlus:Pokemon
+    {
+        public PokemonPlus():base()
+        {
+            this.Power = 0;
+        }
+
+        public PokemonPlus(String name, String description, int power) : base(name, description)
+        {
+            this.Power = power;
+        }
+
+        public int Power { get; set; }
+
+        public void ShowPokemonDataPlus()
+        {
+            Console.WriteLine("Pokemon name: " + this.Name);
+            Console.WriteLine("Pokemon description: " + this.Description + "");
+            Console.WriteLine("Power: " + this.Power);
+        }
+    }
+}
